34cd7bdd1dc0e72aa1dc993f33a6219200308bba
Diana A <dianas14897@gmail.com>
2024-01-05 01:48:44 -0600

    Cambios G13

---
+++ index.js
@@ -0,0 +1,10 @@
+const { ApolloServer } = require("apollo-server");
+const { typeDefs } = require("../schema/type-defs");
+const { resolvers } = require("../schema/resolvers");
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const port = process.env.PORT || 3000;
+
+server.listen(port).then(({ url }) => {
+  console.log(`🚀 API IS RUNNING AT: ${url} `);
+});
