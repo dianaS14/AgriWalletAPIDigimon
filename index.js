c0c45b229846ddd69ad7819220bc9cc6e5d06d2f
Diana A <dianas14897@gmail.com>
2024-01-05 18:48:48 -0600

    config

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./schema/type-defs");
 const { resolvers } = require("./schema/resolvers");
 const server = new ApolloServer({
-  typeDefs, resolvers, introspection: true,
-  playground: true,
+  typeDefs, resolvers
 });
 
 const port = process.env.PORT || 3000;
 
-server.listen(port).then(({ url }) => {
+server.listen({port}).then(({ url }) => {
   console.log(`🚀 API IS RUNNING AT: ${url} `);
 });
