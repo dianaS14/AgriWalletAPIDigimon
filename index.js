2989b6f0fffd0926adcccf21e65994c0d75c8a41
Diana A <dianas14897@gmail.com>
2024-01-05 19:44:57 -0600

    conf

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./schema/type-defs");
 const { resolvers } = require("./schema/resolvers");
